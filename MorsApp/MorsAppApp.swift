20a7192118940b88a53670f5207baebf42fb589e
Mor Naaman <mor.naaman@hellosimply.com>
2025-04-24 12:25:19 +0300

    Initial Commit

---
+++ MorsApp/MorsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorsAppApp.swift
+//  MorsApp
+//
+//  Created by Mor Naaman on 24/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MorsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MorsAppTests/MorsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MorsAppTests.swift
+//  MorsAppTests
+//
+//  Created by Mor Naaman on 24/04/2025.
+//
+
+import Testing
+@testable import MorsApp
+
+struct MorsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
